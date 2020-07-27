5d9ef5a76d6ea938d29627e96a6bee7c726a294b
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-07-27 15:28:29 -0700

    Started GameControl to oversee game

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,9 @@
 class Board:
-
    def __init__(self):
       self.board = [ ["_", "_", "_"],
-                ["_", "_", "_"],
-                ["_", "_", "_"]
-               ]  
+                     ["_", "_", "_"],
+                     ["_", "_", "_"]
+                  ]  
    
    def display(self):
       print("_____________", end='') #9 underscores
@@ -15,6 +14,27 @@ class Board:
    
    def updateBoard(self, move, row, col):
       self.board[row][col] = move
+
+class GameControl:
+   def __init__(self, board, player1Name="Player 1", player2Name = "Player 2"):
+      self.board = board
+      self.currPlayer = player1Name
+      self.nextPlayer = player2Name
+   
+   def updateCurrPlayer(self):
+      currPlayer = player2Name if currPlayer == player1Name else player1Name
+      nextPlayer = player2Name if nextPlayer == player1Name else player2Name
+
+   def makeMove(self):
+      #get user input and update board
+      self.board.display()
+      self.updateCurrPlayer()
+
+   #def gameOver(self):
+      
+
+   #def newGame(self):
+   #   while not(gameOver):
 
 board = Board()
 board.updateBoard("x", 0, 0)
