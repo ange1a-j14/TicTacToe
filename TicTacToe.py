a4299e9785ff87c00cdd74d3db3c7b1c532ef014
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-07-27 22:57:58 -0700

    Works when game ends in win or lose

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,12 +1,9 @@
 class Board:
-   def __init__(self):
-      self.board = [ ["_", "_", "_"],
-                     ["_", "_", "_"],
-                     ["_", "_", "_"]
-                  ]  
+   def __init__(self, board="empty"):
+      self.board = [["_", "_", "_"],["_", "_", "_"],["_", "_", "_"]] if board=="empty" else [["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"]]
    
    def display(self):
-      print("_____________", end='') #9 underscores
+      print("\n_____________", end='') #9 underscores
       for row in self.board:
          print("\n|", end='')
          for el in row:
@@ -15,27 +12,67 @@ class Board:
    def updateBoard(self, move, row, col):
       self.board[row][col] = move
 
+   def checkRows(self):
+      for row in range(3):
+         if self.board[row][0] != "_" and self.board[row][0] == self.board[row][1] and self.board[row][0] == self.board[row][2]:
+            return True
+      return False
+
+   def checkCols(self):
+      for col in range(3):
+         if self.board[0][col] != "_" and self.board[0][col] == self.board[1][col] and self.board[0][col] == self.board[2][col]:
+            return True
+      return False
+
+   def checkDiags(self):
+      return (self.board[0][0] != "_" and self.board[0][0] == board[1][1] and self.board[0][0] == self.board[2][2]) or (self.board[0][2] != "_" and self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0])
+
 class GameControl:
    def __init__(self, board, player1Name="Player 1", player2Name = "Player 2"):
       self.board = board
+      self.player1Name = player1Name
+      self.player2Name = player2Name
       self.currPlayer = player1Name
       self.nextPlayer = player2Name
    
    def updateCurrPlayer(self):
-      currPlayer = player2Name if currPlayer == player1Name else player1Name
-      nextPlayer = player2Name if nextPlayer == player1Name else player2Name
+      temp = self.currPlayer
+      self.currPlayer = self.nextPlayer
+      self.nextPlayer = temp
 
    def makeMove(self):
-      #get user input and update board
+      example = Board("number")
+      position = -1
+      while position < 0 or position > 9:
+         example.display()
+         position = int(input("\n" + self.currPlayer + ", please select a valid position.\n"))
+      rowCol = self.findPosition(position)
+      self.board.updateBoard("x" if self.currPlayer == self.player1Name else "o", rowCol[0], rowCol[1])
       self.board.display()
-      self.updateCurrPlayer()
 
-   #def gameOver(self):
-      
+   #precondition: position E [0, 8]
+   def findPosition(self, position):
+      if position < 3: return [0, position]
+      elif position < 6: return [1, position - 3]
+      else: return [2, position - 6]
 
-   #def newGame(self):
-   #   while not(gameOver):
+   def gameOver(self):
+      if self.board.checkRows() or self.board.checkCols() or self.board.checkDiags():
+         print("\n" + self.nextPlayer, "has won!")
+         return True
+      return False
 
-board = Board()
-board.updateBoard("x", 0, 0)
-board.display()
+   def newGame(self):
+      while not(self.gameOver()):
+         self.makeMove()
+         self.updateCurrPlayer()
+      playAgain = "a"
+      while playAgain != "y" and playAgain != "n":
+         playAgain = input("Would you like to play again? (y/n)")
+      if playAgain == "y": newGame()
+      else: print("Thanks for playing!")
+
+player1 = input("Who is player one?\n")
+player2 = input("Who is player two?\n")
+game = GameControl(Board(), player1, player2)
+game.newGame()
